49f5f08c59ee62d0b1d8a96233bbb29d5acc332f
Silvia Ferrai <silviafer@hotmail.it>
2022-09-21 11:09:49 +0200

    Add api google maps in contact page, create offers page and styling

--- contact.js
+++ contact.js
@@ -1,3 +1,5 @@
+import { comunidades } from "./españa_comunidades.js";
+
 const locations = [
   {
     lat: 36.621031798420155,
@@ -92,19 +94,18 @@ function initMap() {
     disableAutoPan: true,
   });
   const svgMarker = {
-    path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
-    fillColor: "red",
-    fillOpacity: 0.6,
+    path: "M32 0C14.3 0 0 14.3 0 32S14.3 64 32 64V448c-17.7 0-32 14.3-32 32s14.3 32 32 32H208V448h96v64H480c17.7 0 32-14.3 32-32s-14.3-32-32-32V64c17.7 0 32-14.3 32-32s-14.3-32-32-32H32zm80 96h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H112c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16zm112 16c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H240c-8.8 0-16-7.2-16-16V112zM368 96h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H368c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16zM96 208c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H112c-8.8 0-16-7.2-16-16V208zm144-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H240c-8.8 0-16-7.2-16-16V208c0-8.8 7.2-16 16-16zm112 16c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H368c-8.8 0-16-7.2-16-16V208zm-3 152.2c3.3 12.8-7.8 23.8-21 23.8H184c-13.3 0-24.3-10.9-21-23.8c10.6-41.5 48.2-72.2 93-72.2s82.5 30.7 93 72.2z",
+    fillColor: "#BEAD8E",
+    fillOpacity: 1,
     strokeWeight: 0,
     rotation: 0,
-    scale: 2.5,
+    scale: 0.05,
     anchor: new google.maps.Point(15, 30),
+    scaledSize: new google.maps.Size(2, 5),
   };
-  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
   if (!locations) return null;
   const markers = locations.map((position, i) => {
-    const label = labels[i % labels.length];
     let marker = new google.maps.Marker({
       position,
       icon: svgMarker,
@@ -112,24 +113,79 @@ function initMap() {
     });
 
     marker.addListener("click", () => {
-      infoWindow.setContent(label);
       infoWindow.open(map, marker);
     });
     return marker;
   });
   new markerClusterer.MarkerClusterer({ map, markers });
 
-  const locationButton = document.createElement("button");
+  // const locationButton = document.createElement("button");
 
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  // locationButton.textContent = "Pan to Current Location";
+  // locationButton.classList.add("custom-map-control-button");
+  // map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
 
-  function findLocation() {
+  // locationButton.addEventListener("click", () => {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(
+  //       (position) => {
+  //         const pos = {
+  //           lat: position.coords.latitude,
+  //           lng: position.coords.longitude,
+  //         };
+
+  //         infoWindow.setPosition(pos);
+  //         infoWindow.setContent("You are here!");
+  //         infoWindow.open(map);
+  //         map.setCenter(pos);
+  //         userPosition.push(pos);
+  //       },
+  //       () => {
+  //         handleLocationError(true, infoWindow, map.getCenter());
+  //       }
+  //     );
+  //   } else {
+  //     handleLocationError(false, infoWindow, map.getCenter());
+  //   }
+  // });
+
+  geocoder = new google.maps.Geocoder();
+
+  /////////////////////////////
+  const submitButton = document.getElementById("submit");
+  submitButton.addEventListener("click", codeAddress);
+
+  function codeAddress() {
+    address = document.getElementById("input").value;
+    geocoder.geocode({ address: address }, function (results, status) {
+      if (status == "OK") {
+        map.setCenter(results[0].geometry.location);
+        const destinations = locations.map((loc) => ({
+          lat: loc.lat,
+          lng: loc.lng,
+        }));
+        var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location,
+        });
+        calculateDistance(address, destinations);
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+  }
+
+  marker = new google.maps.Marker({
+    map,
+  });
+
+  const buttonRequest = document.getElementById("buttonRequest");
+  let pos = {};
+  buttonRequest.addEventListener("click", () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          const pos = {
+          pos = {
             lat: position.coords.latitude,
             lng: position.coords.longitude,
           };
@@ -138,6 +194,16 @@ function initMap() {
           infoWindow.setContent("You are here!");
           infoWindow.open(map);
           map.setCenter(pos);
+          userPosition.push(pos);
+          const destinations = locations.map((loc) => ({
+            lat: loc.lat,
+            lng: loc.lng,
+          }));
+          const origin = new google.maps.LatLng(
+            userPosition[0].lat,
+            userPosition[0].lng
+          );
+          calculateDistance(origin, destinations);
         },
         () => {
           handleLocationError(true, infoWindow, map.getCenter());
@@ -146,10 +212,7 @@ function initMap() {
     } else {
       handleLocationError(false, infoWindow, map.getCenter());
     }
-  }
-
-  locationButton.addEventListener("click", findLocation);
-
+  });
   function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(
@@ -160,64 +223,8 @@ function initMap() {
     infoWindow.open(map);
   }
 
-  geocoder = new google.maps.Geocoder();
-
-  const inputText = document.getElementById("input");
-
-  const submitButton = document.getElementById("submit");
-
-  marker = new google.maps.Marker({
-    map,
-  });
-  map.addListener("click", (e) => {
-    geocode({ location: e.latLng });
-  });
-  submitButton.addEventListener("click", () =>
-    geocode({ address: inputText.value })
-  );
-}
-
-function geocode(request) {
-  geocoder
-    .geocode(request)
-    .then((result) => {
-      const { results } = result;
-      console.log(results[0].geometry.location);
-      map.setCenter(results[0].geometry.location);
-      marker.setPosition(results[0].geometry.location);
-      marker.setMap(map);
-
-      return results;
-    })
-    .catch((e) => {
-      alert("Geocode was not successful for the following reason: " + e);
-    });
-
-  const buttonRequest = document.createElement("button");
-
-  buttonRequest.textContent = "Find my nearest locations";
-
-  map.controls[google.maps.ControlPosition.LEFT_CENTER].push(buttonRequest);
-  console.log(buttonRequest, "btt");
-  buttonRequest.addEventListener("click", () => {
-    console.log("first");
-  });
-  // if (userPosition.length) {
-  //   const destinations = locations.map((loc) => ({
-  //     lat: loc.lat,
-  //     lng: loc.lng,
-  //   }));
-  //   const origin = new google.maps.LatLng(
-  //     userPosition[0].lat,
-  //     userPosition[0].lng
-  //   );
-  //   calculateDistance(origin, destinations);
-  // } else if (address) {
-  //   calculateDistance(address, destinations);
-  // }
-
   function calculateDistance(origin, destinations) {
-    let service = new google.maps.DistanceMatrixService();
+    const service = new google.maps.DistanceMatrixService();
     service
       .getDistanceMatrix({
         origins: [origin],
@@ -225,7 +232,6 @@ function geocode(request) {
         travelMode: "DRIVING",
       })
       .then((response) => {
-        console.log(response);
         const locations = response.destinationAddresses.map((direction) => ({
           direction: direction,
         }));
@@ -243,16 +249,66 @@ function geocode(request) {
         });
 
         for (let hotel of sortedHotels) {
-          const direction = document.createElement("p");
-          const distance = document.createElement("p");
+          const direction = document.createElement("li");
+          direction.classList.add("direction");
+          const distance = document.createElement("div");
+          distance.classList.add("distance");
           direction.innerText = `${hotel.direction} `;
-          distance.innerText = `${hotel.distance.text}`;
+          distance.innerText = `Distance: ${hotel.distance.text}`;
           document.getElementById("response").appendChild(direction);
           document.getElementById("response").appendChild(distance);
         }
       });
   }
+
+  const select = document.getElementById("comunidadesAuto");
+  comunidadesAutonomas.map((item) => {
+    const comunidad = document.createElement("option");
+    (comunidad.textContent = item),
+      (comunidad.value = item),
+      select.appendChild(comunidad);
+  });
+
+  select.addEventListener("change", () => {
+    selectComunidad();
+  });
+
+  function selectComunidad() {
+    let index = select.selectedIndex;
+
+    let marker = new google.maps.Polygon({
+      paths: comunidades[index - 1],
+      strokeColor: "#135846",
+      strokeOpacity: 1,
+      strokeWeight: 2,
+      fillColor: "#5AD07A",
+      fillOpacity: 0.8,
+    });
+    marker.setMap(map);
+  }
 }
-console.log(userPosition, "user");
 
 window.initMap = initMap;
+// Comunidades autonomas:
+
+const comunidadesAutonomas = [
+  "Andalucía",
+  "Aragón",
+  "Asturias, Principado de",
+  "Balears, Illes",
+  "Canarias",
+  "Cantabria",
+  "Castilla y León",
+  "Castilla - La Mancha",
+  "Cataluña / Catalunya",
+  "Comunitat Valenciana",
+  "Extremadura",
+  "Galicia",
+  "Madrid, Comunidad de",
+  "Murcia, Región de",
+  "Navarra, Comunidad Foral de",
+  "País Vasco / Euskadi",
+  "Rioja, La",
+  "Ceuta",
+  "Melilla",
+];
