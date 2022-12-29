063d1f7f5f06b28c408592f978a51a5614d6f09a
Silvia Ferrai <silviafer@hotmail.it>
2022-12-29 12:47:28 +0100

    Add keys search options

--- js/search.js
+++ js/search.js
@@ -2,20 +2,47 @@
 
 function Search() {
   const search_query = document.getElementById("search-bar").value;
-  if (search_query.toLowerCase().includes("room", "rooms")) {
+  if (
+    search_query.toLowerCase().includes("rooms") ||
+    search_query.toLowerCase().includes("duplex") ||
+    search_query.toLowerCase().includes("minimal")
+  ) {
     window.location.assign("./rooms.html");
-    search_query = "";
-  } else if (search_query.toLowerCase().includes("about")) {
+  } else if (
+    search_query.toLowerCase().includes("about") ||
+    search_query.toLowerCase().includes("video") ||
+    search_query.toLowerCase().includes("hotel") ||
+    search_query.toLowerCase().includes("miranda")
+  ) {
     window.location.assign("./about-us.html");
   } else if (search_query.toLowerCase().includes("contact")) {
     window.location.assign("./contact.html");
   } else if (search_query.toLowerCase().includes("offers")) {
     window.location.assign("./offers.html");
-  } else if (search_query.toLowerCase().includes("home")) {
+  } else if (
+    search_query.toLowerCase().includes("home") ||
+    search_query.toLowerCase().includes("features")
+  ) {
     window.location.assign("./index.html");
-  } else if (search_query.toLowerCase().includes("booking")) {
+  } else if (
+    search_query.toLowerCase().includes("book") ||
+    search_query.toLowerCase().includes("luxury")
+  ) {
     window.location.assign("./room-details.html");
+  } else if (
+    search_query.toLowerCase().includes("map") ||
+    search_query.toLowerCase().includes("address") ||
+    search_query.toLowerCase().includes("phone") ||
+    search_query.toLowerCase().includes("email")
+  ) {
+    window.location.assign(`./contact.html#${search_query}`);
+  } else if (
+    search_query.toLowerCase().includes("services") ||
+    search_query.toLowerCase().includes("menu") ||
+    search_query.toLowerCase().includes("food")
+  ) {
+    window.location.assign(`./index.html#${search_query}`);
   } else {
-    // alert("No matches found");
+    return false;
   }
 }
